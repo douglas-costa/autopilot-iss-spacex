94c552ce2ec37e34e63a4cde4ee61e730378e40a
Douglas da Costa <douglas-costa@users.noreply.github.com>
2020-06-14 22:05:19 -0300

    Implementing Z translation

--- autopilot.js
+++ autopilot.js
@@ -126,11 +126,31 @@ class YController extends Controller {
     }
 }
 
+class ZController extends Controller {
+    constructor(decreaseButton, increaseButton) {
+        super(decreaseButton, increaseButton)
+    }
+
+    adjust() {
+        if (this.error >= -0.1 && this.error <= 0.1) {
+            this.stop()
+        }
+        else if (this.error < 0) {
+            this.increase()
+        }
+        else if (this.error > 0) {
+            this.decrease()
+        }
+    }
+}
+
 (function() {
     let rollController  = new RollController($('#roll-left-button'), $('#roll-right-button'))
     let yawController   = new YAWController($('#yaw-left-button'), $('#yaw-right-button'))
     let pitchController = new PitchController($('#pitch-down-button'), $('#pitch-up-button'))
+
     let yController     = new YController($('#translate-left-button'), $('#translate-right-button'))
+    let zController     = new ZController($('#translate-down-button'), $('#translate-up-button'))
 
     setInterval(() => {
         let rollError  = parseFloat($('#roll > .error').innerText.slice(0, -1))
@@ -138,11 +158,13 @@ class YController extends Controller {
         let pitchError = parseFloat($('#pitch > .error').innerText.slice(0, -1))
 
         let yDistance  = parseFloat($('#y-range > .distance').innerText.slice(0, -1))
+        let zDistance  = parseFloat($('#z-range > .distance').innerText.slice(0, -1))
 
         rollController.setError(rollError).adjust()
         yawController.setError(yawError).adjust()
         pitchController.setError(pitchError).adjust()
 
         yController.setError(yDistance).adjust()
+        zController.setError(zDistance).adjust()
     }, 500)
 })()
