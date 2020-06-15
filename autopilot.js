3f7c510a2613c5309490f87261028f800a3a0129
Douglas da Costa <douglas-costa@users.noreply.github.com>
2020-06-14 21:15:48 -0300

    Refactoring main controller and implementing YAW controller

--- autopilot.js
+++ autopilot.js
@@ -1,77 +1,112 @@
-(function() {
-    class RollController {
-        constructor() {
-            this.error              = 0
-            this.leftButton         = $('#roll-left-button')
-            this.leftButtonClicked  = false
-            this.rightButton        = $('#roll-right-button')
-            this.rightButtonClicked = false
-        }
-
-        getError() {
-            return this.error
-        }
-
-        setError(error) {
-            this.error = error
-        }
-
-        isRotatingToLeft() {
-            return this.leftButtonClicked
-        }
-
-        isRotatingToRight() {
-            return this.rightButtonClicked
-        }
-
-        rollToLeft() {
-            if (this.isRotatingToLeft()) {
-                return
-            }
-
-            this.leftButtonClicked  = true
-            this.rightButtonClicked = false
-            this.leftButton.click()
-        }
-
-        rollToRight() {
-            if (this.isRotatingToRight()) {
-                return
-            }
-
-            this.rightButtonClicked = true
-            this.leftButtonClicked  = false
-            this.rightButton.click()
-        }
-
-        stopRotating() {
-            if (this.isRotatingToLeft()) {
-                this.rightButton.click()
-            }
-            else if (this.isRotatingToRight()) {
-                this.leftButton.click()
-            }
-
-            this.leftButtonClicked  = false
-            this.rightButtonClicked = false
-        }
+class Controller {
+    constructor(config = {}) {
+        this.error          = 0
+        this.decreaseButton = config.decreaseButton
+        this.decreasing     = false
+        this.increaseButton = config.increaseButton
+        this.increasing     = false
+        this.stopped        = false
     }
 
-    let rollController = new RollController()
+    setError(error) {
+        this.error = error
+    }
+
+    decrease() {
+        if (this.decreasing) {
+            return
+        }
+
+        this.decreasing = true
+        this.increasing = false
+        this.decreaseButton.click()
+    }
+
+    increase() {
+        if (this.increasing) {
+            return
+        }
+
+        this.increasing = true
+        this.decreasing = false
+        this.increaseButton.click()
+    }
+
+    stop() {
+        if (this.stopped) {
+            return
+        }
+
+        if (this.increasing) {
+            this.decreaseButton.click()
+        }
+        else if (this.decreasing) {
+            this.increaseButton.click()
+        }
+
+        this.decreasing = false
+        this.increasing = false
+        this.stopped    = true
+    }
+
+    adjust() {}
+}
+
+class RollController extends Controller {
+    constructor(config = {}) {
+        super(config)
+    }
+
+    adjust() {
+        if (this.error >= -0.1 && this.error <= 0.1) {
+            this.stop()
+        }
+        else if (this.error < 0) {
+            this.decrease()
+        }
+        else if (this.error > 0) {
+            this.increase()
+        }
+    }
+}
+
+class YAWController extends Controller {
+    constructor(config = {}) {
+        super(config)
+    }
+
+    adjust() {
+        if (this.error >= -0.1 && this.error <= 0.1) {
+            this.stop()
+        }
+        else if (this.error < 0) {
+            this.increase()
+        }
+        else if (this.error > 0) {
+            this.decrease()
+        }
+    }
+}
+
+(function() {
+    let rollController = new RollController({
+        decreaseButton: $('#roll-left-button'),
+        increaseButton: $('#roll-right-button')
+    })
+
+    let yawController = new YAWController({
+        decreaseButton: $('#yaw-right-button'),
+        increaseButton: $('#yaw-left-button')
+    })
 
     setInterval(() => {
         let rollError = parseFloat($('#roll > .error').innerText.slice(0, -1))
+        let yawError  = parseFloat($('#yaw > .error').innerText.slice(0, -1))
 
         rollController.setError(rollError)
+        rollController.adjust()
 
-        if (rollController.getError() >= -0.1 && rollController.getError() <= 0.1) {
-            rollController.stopRotating()
-        }
-        else if (rollController.getError() < 0) {
-            rollController.rollToLeft();
-        }
-        else if (rollController.getError() > 0) {
-            rollController.rollToRight();
-        }
+        yawController.setError(yawError)
+        yawController.adjust()
     }, 500)
 })()
