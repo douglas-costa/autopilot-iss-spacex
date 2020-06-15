db0e533b397bedc701d70849aef1195743679cad
Douglas da Costa <douglas-costa@users.noreply.github.com>
2020-06-14 21:52:56 -0300

    Implementing Y translation

--- autopilot.js
+++ autopilot.js
@@ -108,18 +108,41 @@ class PitchController extends Controller {
     }
 }
 
+class YController extends Controller {
+    constructor(decreaseButton, increaseButton) {
+        super(decreaseButton, increaseButton)
+    }
+
+    adjust() {
+        if (this.error >= -0.1 && this.error <= 0.1) {
+            this.stop()
+        }
+        else if (this.error < 0) {
+            this.increase()
+        }
+        else if (this.error > 0) {
+            this.decrease()
+        }
+    }
+}
+
 (function() {
     let rollController  = new RollController($('#roll-left-button'), $('#roll-right-button'))
     let yawController   = new YAWController($('#yaw-left-button'), $('#yaw-right-button'))
     let pitchController = new PitchController($('#pitch-down-button'), $('#pitch-up-button'))
+    let yController     = new YController($('#translate-left-button'), $('#translate-right-button'))
 
     setInterval(() => {
         let rollError  = parseFloat($('#roll > .error').innerText.slice(0, -1))
         let yawError   = parseFloat($('#yaw > .error').innerText.slice(0, -1))
         let pitchError = parseFloat($('#pitch > .error').innerText.slice(0, -1))
 
+        let yDistance  = parseFloat($('#y-range > .distance').innerText.slice(0, -1))
+
         rollController.setError(rollError).adjust()
         yawController.setError(yawError).adjust()
         pitchController.setError(pitchError).adjust()
+
+        yController.setError(yDistance).adjust()
     }, 500)
 })()
